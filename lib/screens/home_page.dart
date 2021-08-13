d10362e1a73cb95a4c2e4f1bd073320d84d2791f
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-08-13 13:50:04 +0600

    body layout

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,3 +13,26 @@
       },
     );
   }
+  _buildBody(List<VideoModel>? videoModel) {
+    return ListView.builder(
+      itemCount: _videoModel!.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) =>
+                  VideoScreen(id: _videoModel![index].id.toString()),
+            ),
+          ),
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(
+                  videoModel![index].snippet.thumbnails.thumbnailsDefault.url),
+            ),
+          ),
+        );
+      },
+    );
+  }
